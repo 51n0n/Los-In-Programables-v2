7c17907f990564b1ddd03afaecdb09cace7daa9e
Nico <ncuadra19@alumnos.utalca.cl>
2022-09-01 23:13:10 -0400

    Reemplazo de Clases FXML a Clases POO
    Nuevas Clases "Caracter" y "CrearCaracteres", inicialización de atributos y métodos en "Caracter" y eliminación de clases FXML y Controller.

---
+++ src/clases/CrearCaracteres.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import javafx.scene.layout.AnchorPane;
+
+public class CrearCaracteres {
+    
+    public void crear_A() {
+        
+        
+        
+        
+    }
+    
+}
