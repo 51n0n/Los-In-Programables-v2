4c360da25e3444952e49b773c490b0b27f50b509
Franco Salas <fsalas19@alumnos.utalca.cl>
2022-09-04 16:44:11 -0300

    Actualización de interfaz y controller

--- src/application/AppTipografia.java
+++ src/application/AppTipografia.java
@@ -9,7 +9,6 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
--- src/tipografias/script/fluida/FXMLDocumentController.java
+++ src/tipografias/script/fluida/FXMLDocumentController.java
@@ -9,7 +9,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,11 +21,21 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button buttonFinalizar;
+    @FXML
+    private Button buttonIniciar;
+    @FXML
+    private TextField ingresarPalabra;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonIniciar(ActionEvent event) {
+        System.out.println("Inicio el proceso de dibujo");
+    }
+    
+    @FXML
+    private void handleButtonFinalizar(ActionEvent event) {
+        System.out.println("Finalizo el programa");
     }
     
     @Override
