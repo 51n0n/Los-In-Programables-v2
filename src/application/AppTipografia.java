4945b6bbe5d8bde3dff79e75fde8e1a9dd207be1
Franco Salas <fsalas19@alumnos.utalca.cl>
2022-09-04 19:01:46 -0300

    Actualización AppTipografia.java
    Cambios en start y nuevo método drawLetter

--- src/application/AppTipografia.java
+++ src/application/AppTipografia.java
@@ -10,61 +10,46 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.QuadCurve;
+import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
 public class AppTipografia extends Application {
     
-    /**
-     *
-     * @param primaryStage
-     * @throws java.lang.Exception
-     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start (Stage primaryStage) throws Exception{
+        
+        //Titulo de la ventana
+        primaryStage.setTitle("Tipografia Script Fluida");
         
         //Creacion de la ventana y contenedores principales
         Group root = new Group();
         
         //Creacion de zona de dibujo
-        Canvas canvas = new Canvas(200, 150);
+        Canvas canvas = new Canvas(300, 250);
         
         //Obtención del canvas
         GraphicsContext gc = canvas.getGraphicsContext2D();
         
+        drawLetter(gc);
+        
         //Se añade el canvas al contenedor principal
         root.getChildren().add(canvas);
         
-        //Crea escena de 600x400, gris claro
-        Scene scene = new Scene(root, 600, 400, Color.WHITE);
-        primaryStage.setScene(scene);
+        //Crea escena
+        primaryStage.setScene(new Scene(root));
         
-        //Titulo de la ventana
-        primaryStage.setTitle("Tipografia Script Fluida");
-        
+        //Muestra la escena
         primaryStage.show();
-        
-        //DIBUJO
-        //Dibuja linea desde la posicion (x,y) a (x1, y1)
+    }
+    
+    private void drawLetter(GraphicsContext gc){
+        gc.setFill(Color.BLACK);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(3);
         gc.strokeLine(40, 10, 10, 40);
-        
-        /*
-        QuadCurve quad=new QuadCurve();
-        quad.setStartX(100);
-        quad.setStartY(220);
-        quad.setControlX(250);
-        quad.setControlY(0);
-        quad.setEndX(500);
-        quad.setEndY(220);
-        
-        root.getChildren().add(quad);*/
     }
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
 }
