8395cda8028d9d01286791644972ceff4da0d933
Nico <ncuadra19@alumnos.utalca.cl>
2022-09-05 16:03:56 -0300

    Actualización GridPane

--- src/application/AppTipografia.java
+++ src/application/AppTipografia.java
@@ -6,6 +6,7 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -14,21 +15,38 @@ public class AppTipografia extends Application {
     
     private static BorderPane nodoRaiz = new BorderPane();
     private static CrearCaracteres aux = new CrearCaracteres();
+    private static AnchorPane aux2 = new AnchorPane();
     
     @Override
     public void start(Stage ventana) throws Exception {
         
-        nodoRaiz.setPrefHeight(650.0);
-        nodoRaiz.setPrefWidth(1300.0);
+        nodoRaiz.setPrefSize(1300,650);
         
         GridPane tabla = new GridPane();
-        tabla.setPrefHeight(500);
-        tabla.setPrefWidth(300);
+        tabla.setPrefSize(500,500);
         nodoRaiz.getChildren().add(tabla);
+        aux2 = aux.crear_A();
         
-        nodoRaiz.getChildren().add(aux.crear_A());
+        //nodoRaiz.getChildren().add(aux2); //funciona sin esto
         
+        tabla.addRow(0, aux2);
+        tabla.setLayoutX(200);
+        tabla.setLayoutY(200);
         
+        AnchorPane a1 = new AnchorPane();
+        AnchorPane a2 = new AnchorPane();
+        AnchorPane a3 = new AnchorPane();
+        a1.setPrefSize(45, 60);
+        a2.setPrefSize(45, 60);
+        a3.setPrefSize(45, 60);
+        //tabla.add(a1, 0, 1);
+        //tabla.add(a2, 1, 0);
+        //tabla.add(a3, 1, 1);
+        tabla.addRow(0, a1);
+        tabla.addRow(0, a2);
+        tabla.addRow(0, a3);
+        
+        tabla.setGridLinesVisible(true);
         //Mostrar Escena
         Scene escena = new Scene(nodoRaiz);
         ventana.setScene(escena);
