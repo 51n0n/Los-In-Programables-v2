516c6b4a09d4219dca2715a370383059a586441c
Nico <ncuadra19@alumnos.utalca.cl>
2022-10-22 17:19:32 -0300

    Título en Interfaz

--- src/application/InterfazController.java
+++ src/application/InterfazController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 public class InterfazController implements Initializable {
@@ -26,6 +27,10 @@ public class InterfazController implements Initializable {
     private ColorPicker selectColor;
     @FXML
     private CheckBox CheckPuntos;
+    @FXML
+    private BorderPane encabezado;
+    @FXML
+    private AnchorPane titulo;
     
     Dibujar dibujar = new Dibujar(); // Objeto de la clase Dibujar
     
@@ -34,13 +39,15 @@ public class InterfazController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        dibujar.guardarPalabras("^NLOS ^NIN-PROGRAMABLES",titulo);
+        
         dibujar.getLienzo().setStyle("-fx-background-color: LIGHTGRAY;-fx-border-color: BLACK");
-        //dibujar.getLienzo().setStyle("-fx-border-width: 2");
         dibujar.getLienzo().setLayoutX(0);
         dibujar.getLienzo().setLayoutY(0);
         dibujar.getLienzo().setPrefSize(1070, 450);
         dibujar.getLienzo().setMaxSize(1070, 450);
         fondoInterfaz.getChildren().add(dibujar.getLienzo()); // Se añade el lienzo de dibujo
+        
         textoEntrada.setOnKeyTyped((KeyEvent event) -> {
             if (!dibujar.validarEntrada(event.getCharacter())){
                 event.consume();
@@ -49,8 +56,7 @@ public class InterfazController implements Initializable {
         
         textoEntrada.setOnKeyReleased((KeyEvent event) -> {
             dibujar.getLienzo().getChildren().clear();
-            //dibujar.dibujarEntrada(textoEntrada.getText()); // Se dibuja
-            dibujar.guardarPalabras(textoEntrada.getText());
+            dibujar.guardarPalabras(textoEntrada.getText(),dibujar.getLienzo());
         });
     }
     
@@ -60,14 +66,14 @@ public class InterfazController implements Initializable {
         Color nuevoColor = selectColor.getValue(); // Se obtiene el valor de color del color picker de la interfaz
         dibujar.setColor(nuevoColor); // Se llama al setter del color para las letras y se asigna el color seleccionado en la interfaz
         dibujar.getLienzo().getChildren().clear();
-        dibujar.guardarPalabras(textoEntrada.getText());
+        dibujar.guardarPalabras(textoEntrada.getText(),dibujar.getLienzo());
     }
     
     @FXML
     private void puntosControl (ActionEvent event){
         dibujar.setControl(CheckPuntos.isSelected());
         dibujar.getLienzo().getChildren().clear();
-        dibujar.guardarPalabras(textoEntrada.getText());
+        dibujar.guardarPalabras(textoEntrada.getText(),dibujar.getLienzo());
     }
     
 }
